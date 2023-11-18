9e3650dce6406cb2d047c9575ee21a33effe693c
Umirzoq <umirzoq@gmail.com>
2023-11-18 17:33:34 +0500

    2-modul functional components

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,24 @@
-import React, { Component } from 'react'
-import Cabinet from './Pages/Cabinet'
-import Product from './Pages/Product'
-import { Link, Switch, Route } from 'react-router-dom'
-import Setting from './Pages/Setting'
-import Dashboard from './Pages/Dashboard'
-import Balance from './Pages/Balance'
-import OneProduct from './Pages/OneProduct'
-import Details from './Pages/Details'
-import LastPage from './Pages/LastPage'
-export default class App extends Component {
-  render() {
-    return (
-      <div className='container'>
-        <div className="row my-3">
-          <div className="col-md-12">
-            <Link to="/" className='btn btn-dark mx-1'>Home</Link>
-            <Link to="/cabinet" className='btn btn-success'>Cabinet</Link>
-            <Link to="/product" className='btn btn-success mx-1'>Product</Link>
-          </div>
-        </div>
-        <hr />
-        <Switch>
-          <Route path='/product/:id/setting/details/:id' component={LastPage} />
-          <Route path='/product/:id/setting/details' component={Details} />
-          <Route path='/product/:id/setting' component={Setting} />
-          <Route path='/product/:id' component={OneProduct} />
-          <Route path='/cabinet/dashboard/balance' component={Balance} />
-          <Route path='/cabinet/dashboard' component={Dashboard} />
-          <Route path='/cabinet/setting' component={Setting} />
-          <Route path='/cabinet' component={Cabinet} />
-          <Route path='/product' component={Product} />
-        </Switch>
-      </div>
-    )
+import { useState } from "react"
+
+const App = () => {
+
+  const [user, setUser] = useState({ firstName: "AAA", lastName: "BBB"})
+
+  var ChangeName = () => {
+    user.firstName="Ulugbek"
+    
+    var a = {...user}
+
+    setUser(a)
   }
+  return (
+    <div className="container">
+      { console.log(user)}
+      <h1>{user.firstName + " " + user.lastName}</h1>
+
+      <button onClick={ChangeName}>changeName</button>
+    </div>
+  )
 }
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import ReactDOM from 'react-dom/client'
 import App from "./App";
-import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <Router>
-        <App/>
-    </Router>
+        <App value={"AAAAA"}/>
 )
